966aae6a8407ee6f52dc4999895690a90ba38529
gracea18 <72771341+gracea18@users.noreply.github.com>
2020-11-17 11:18:38 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 while True:
-    print("Temperature (F): " + input.temperature(TemperatureUnit.FAHRENHEIT))
-    print("Temperature (C): " + input.temperature(TemperatureUnit.CELSIUS))
+    print("Temperature(F): " + input.temperature(TemperatureUnit.FAHRENHEIT) + " - Temperature (C): " + input.temperature(TemperatureUnit.CELSIUS))
     
