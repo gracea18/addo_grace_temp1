5bf093a697596ee0357c7855a803dbd143d70699
gracea18 <72771341+gracea18@users.noreply.github.com>
2020-11-19 13:30:44 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+tempF=68
 while True:
     print(" Current Room Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT) + input.temperature(TemperatureUnit.CELSIUS))
+
+if tempF> 60:
+    light.set_pixel_color(5, light.rgb (255, 0, 0))
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let tempF = 68
 while (true) {
     console.log(" Current Room Temperature: " + input.temperature(TemperatureUnit.Fahrenheit) + input.temperature(TemperatureUnit.Celsius))
 }
+if (tempF > 60) {
+    light.setPixelColor(5, light.rgb(255, 0, 0))
+}
+
